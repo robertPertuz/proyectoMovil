f5c656fb30a37d9c795468d30bfdc9dbebc4b414
robertPertuz <ralbertopertuz@unicesar.edu.co>
2023-04-20 16:55:59 -0500

    get

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'auth/login.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Crud Usando HTTP',
+      theme: ThemeData(primarySwatch: Colors.green),
+      initialRoute: '/login',
+      routes: {
+        "/login": (context) => const Login(),
+      },
+    );
   }
 }
